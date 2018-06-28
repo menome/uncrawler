93550b325d34a22f174f6277627ccbd9a802e23f
Konrad Aust <konrad.aust@gmail.com>
2018-06-28 13:12:54 -0600

    Now streams file results from neo4j, rather than handling all at once

--- app/uncrawler.js
+++ app/uncrawler.js
@@ -3,53 +3,61 @@
  *
  * Crawls a file tree, copies it, and turns it into graph nodes.
  */
-const createClient = require('webdav');
 const async = require('async');
-const rp = require('request-promise')
 
 const fileQuery = "MATCH (f:File) WHERE EXISTS(f.LibraryKey) AND EXISTS(f.LibraryPath) RETURN f.Uuid as uuid, f.LibraryKey as key, f.LibraryPath as path";
 const delQuery = "MATCH (f:File {Uuid: $uuid}) DETACH DELETE f";
 
-module.exports = function crawler(bot) {
-  this.uncrawl = function() {
-
-
-    var workQueue = async.queue((task,callback) => {
-      task().then((resp)=>{
-        console.log(resp);
-        callback();
-      })
-    })
-  
-    // Called when our work queue is empty.
-    // If our work queue is empty AND our crawling command has finished, then we're done here.
-    workQueue.drain = () => {
-      console.log("Work Queue Empty.")
-      // if(findFinished === true) {
-      //   bot.logger.info("Finished crawling",localCrawlDir);
-      //   return cb();
-      // }
-    }
-    // var deletedFiles = [];
-
-    return bot.neo4j.query(fileQuery).then((result) => {
-      result.records.forEach((rec) => {
-        workQueue.push(() => {
-          return bot.librarian.stat(rec.get('key'),rec.get('path')).catch((err) => {
-            if(err.statusCode === 404) {
-              bot.logger.info("File does not exist", rec.get('path'));
-
-              return bot.neo4j.query(delQuery, {uuid: rec.get('uuid')}).then((result) => {
-                bot.logger.info("Deleted file ", rec.get('uuid'))
-              })
-            }
-            else 
-              bot.logger.info("Could not get file.")
-          })
-        })
-      })
+module.exports = function uncrawler(bot) {
+  // Keep one single workqueue for the uncrawler instance.
+  var workQueue = async.queue((task,callback) => {
+    task().then((resp)=>{
+      bot.logger.info(resp);
+      callback();
     }).catch((err) => {
-      bot.logger.error("Could not fetch files:", err.toString());
+      bot.logger.error(err.toString())
+      callback();
+    })
+  })
+
+  // Called when our work queue is empty.
+  // Probably not necessary. We maintain one work queue. 
+  // When it drains we don't move on to other logic. We just wait for more things.
+  workQueue.drain = () => {
+    console.log("Work Queue Empty.")
+  }
+
+  this.uncrawl = function() {
+    var sesh = bot.neo4j.session();
+
+    return new Promise((resolve,reject) => {
+      sesh.run(fileQuery).subscribe({
+        onNext: function(rec) {
+          workQueue.push(() => {
+            return bot.librarian.stat(rec.get('key'),rec.get('path')).then(() => {
+              return "Keeping file " + rec.get('uuid')
+            }).catch((err) => {
+              if(err.statusCode === 404) {
+                return bot.neo4j.query(delQuery, {uuid: rec.get('uuid')}).then(() => {
+                  return "Deleted File " + rec.get('uuid');
+                })
+              }
+              else {
+                throw new Error("Could not get file: " + rec.get('uuid'))
+              }
+            })
+          })
+        },
+        onError: function(err) {
+          bot.logger.error("Could not fetch files:", err.toString());
+          sesh.close();
+          reject(err);
+        },
+        onCompleted: function() {
+          sesh.close();
+          resolve("All checkable file UUIDs retrieved.")
+        }
+      })
     });
   }
 }
